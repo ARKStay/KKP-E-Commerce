3a0dc0b41d35d8198ce770643e83bbc352460814
ARKStay <152597208+ARKStay@users.noreply.github.com>
2025-01-05 10:32:03 +0700

    Add files via upload

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+<x-layouts.layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <div class="space-y-4 mt-10">
+        <a href="/dashboard" class="px-6 py-3 text-white bg-blue-500 rounded-lg hover:bg-blue-600">Dashboard
+            Admin</a>
+    </div>
+</x-layouts.layout>
---
+++ resources/views/dashboard/Payments/index.blade.php
@@ -0,0 +1,155 @@
+<x-dashboard.layout>
+    <x-slot:title>Payments</x-slot:title>
+    <div class="px-4 mx-auto max-w-screen-2xl lg:px-12">
+        <div class="relative overflow-hidden bg-white shadow-md sm:rounded-lg">
+
+            <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+                <div class="w-full md:w-1/2">
+                    <form class="flex items-center">
+                        <label for="search" class="sr-only">Search</label>
+                        <div class="relative w-full">
+                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="currentColor"
+                                    viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <input type="text" id="search" name="search" value="{{ request('search') }}"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2"
+                                placeholder="Search" autocomplete="off">
+                        </div>
+                        <button type="submit"
+                            class="ml-2 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
+                            Search
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div class="overflow-x-auto">
+                <table class="w-full text-sm text-left text-gray-500">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                        <tr>
+                            <th scope="col" class="px-4 py-3"></th>
+                            <th scope="col" class="px-4 py-3">No.</th>
+                            <th scope="col" class="px-4 py-3">Payment Proof</th>
+                            <th scope="col" class="px-4 py-3">Order Number</th>
+                            <th scope="col" class="px-4 py-3">User Name</th>
+                            <th scope="col" class="px-4 py-3">Payment Status</th>
+                            <th scope="col" class="px-4 py-3 whitespace-nowrap">Last Update</th>
+                            <th scope="col" class="px-4 py-3">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($orders as $order)
+                            <tr class="border-b hover:bg-gray-100">
+                                <td class="px-4 py-3 font-medium text-gray-900"></td>
+                                <td class="px-4 py-3 font-medium text-gray-900">{{ $loop->iteration }}</td>
+                                <td class="px-4 py-3 font-medium text-gray-900">
+                                    @if ($order->image)
+                                        <img src="{{ asset('storage/' . $order->image) }}" alt="Payment Proof"
+                                            class="w-20 h-20 object-cover">
+                                    @else
+                                        No proof
+                                    @endif
+                                </td>
+                                <td class="px-4 py-3 font-medium text-gray-900">{{ $order->cart_id }}</td>
+                                <td class="px-4 py-3 font-medium text-gray-900">{{ $order->user->name }}</td>
+                                <td class="px-4 py-3 font-medium">
+                                    @if ($order->payment_status === 'pending')
+                                        <span class="text-yellow-500">Pending</span>
+                                    @elseif ($order->payment_status === 'approved')
+                                        <span class="text-green-500">Approved</span>
+                                    @elseif ($order->payment_status === 'rejected')
+                                        <span class="text-red-500">Rejected</span>
+                                    @else
+                                        <span class="text-gray-500">Unknown</span>
+                                    @endif
+                                </td>
+                                <td class="px-4 py-3 font-medium text-gray-900">
+                                    {{ $order->updated_at->format('d M Y H:i') }}</td>
+                                <td class="px-4 py-3 font-medium">
+                                    <div class="flex flex-col space-y-2">
+                                        <form action="" method="POST">
+                                            @csrf
+                                            @method('PATCH')
+                                            <button type="submit"
+                                                class="w-full bg-green-500 text-white px-2 py-1 rounded-lg hover:bg-green-600">
+                                                Approve
+                                            </button>
+                                        </form>
+                                        <form action="" method="POST">
+                                            @csrf
+                                            @method('PATCH')
+                                            <button type="submit"
+                                                class="w-full bg-red-500 text-white px-2 py-1 rounded-lg hover:bg-red-600">
+                                                Reject
+                                            </button>
+                                        </form>
+                                    </div>
+                                </td>
+
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <nav class="flex flex-col items-start justify-between p-4 space-y-3 md:flex-row md:items-center md:space-y-0 mt-8"
+                aria-label="Table navigation">
+                <span class="text-sm font-normal text-gray-500">
+                    Showing
+                    <span class="font-semibold text-gray-900">1-10</span>
+                    of
+                    <span class="font-semibold text-gray-900">1000</span>
+                </span>
+                <ul class="inline-flex items-stretch -space-x-px">
+                    <li>
+                        <a href="#"
+                            class="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
+                            <span class="sr-only">Previous</span>
+                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-center justify-center px-3 py-2 text-sm leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">1</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-center justify-center px-3 py-2 text-sm leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">2</a>
+                    </li>
+                    <li>
+                        <a href="#" aria-current="page"
+                            class="z-10 flex items-center justify-center px-3 py-2 text-sm leading-tight border text-primary-600 bg-primary-50 border-primary-300 hover:bg-primary-100 hover:text-primary-700">3</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-center justify-center px-3 py-2 text-sm leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">...</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-center justify-center px-3 py-2 text-sm leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">100</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
+                            <span class="sr-only">Next</span>
+                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</x-dashboard.layout>
